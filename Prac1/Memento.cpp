406007233491ee6116f0a15c819fa62d019dacab
bandilem-git <u24675394@tuks.co.za>
2025-08-09 13:29:14 +0200

    uses cloning function to make deep copies

--- Prac1/Memento.cpp
+++ Prac1/Memento.cpp
@@ -1,4 +1,8 @@
 #include "Memento.h"
 
+#include "Caretaker.h"
 // Constructor
-Memento::Memento(Shape* elements) : shapes(elements){} // not sure if it even matters if it's null or not 
+Memento::Memento(Shape* elements){
+    shapes = elements->clone(); // accurate copies
+    
+} // not sure if it even matters if it's null or not 
