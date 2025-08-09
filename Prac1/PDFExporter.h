0b5bf8f1d32e236a029da595aee611cc7833ddae
bandilem-git <u24675394@tuks.co.za>
2025-08-09 14:11:41 +0200

    Exporter classes partially initialised

--- Prac1/PDFExporter.h
+++ Prac1/PDFExporter.h
@@ -1,5 +1,10 @@
 #ifndef PDFEXPORTER_H
 #define PDFEXPORTER_H
 
+#include "ExportCanvas.h"
+
+class PDFExporter: public ExportCanvas{
+
+};
 
 #endif
--- Prac1/PNGExporter.h
+++ Prac1/PNGExporter.h
@@ -1,4 +1,9 @@
 #ifndef PNGEXPORTER_H
 #define PNGEXPORTER_H
 
+#include "ExportCanvas.h";
+class PDFExporter: public ExportCanvas{
+
+};
+
 #endif
