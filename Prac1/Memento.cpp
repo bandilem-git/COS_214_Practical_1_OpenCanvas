5ff9f9fa80f179de839c9f9acadc3125bee2c3ee
bandilem-git <u24675394@tuks.co.za>
2025-08-09 14:07:13 +0200

    removed an invalid #include

--- Prac1/Memento.cpp
+++ Prac1/Memento.cpp
@@ -1,8 +1,7 @@
 #include "Memento.h"
 
-#include "Caretaker.h"
 // Constructor
 Memento::Memento(Shape* elements){
-    shapes = elements->clone(); // accurate copies
+    shapes = elements->clone(); // accurate copies (Debatable)
     //make it save
 } // not sure if it even matters if it's null or not 
